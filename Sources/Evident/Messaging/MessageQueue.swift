e7fea0fa503a4128dd6e7a8929d7b8e5eddb74f4
Steven Grosmark <steve@g-mark.com>
2026-03-11 11:30:48 -0400

    Rework MessageQueue to use an AsyncStream

--- Sources/Evident/Messaging/MessageQueue.swift
+++ Sources/Evident/Messaging/MessageQueue.swift
@@ -8,18 +8,48 @@
 import Foundation
 
 /// General purpose message queue, supporting cancellable observations.
-public actor MessageQueue<Value: Sendable> {
+///
+/// Thread safe, one-to-many messaging.
+///
+/// Dispatch from anywhere:
+/// ```swift
+/// messageQueue.dispatch("hello")
+/// ```
+///
+/// Set up a forever observation:
+/// ```swift
+/// messageQueue.observe { message in
+///     print(message)
+/// }
+/// ```
+///
+/// Set up a cancellable observation:
+/// ```swift
+/// class Thingy {
+///     private var cancellable: AnyCancellableAsync
+///
+///     init() {
+///         cancellable = messageQueue.observe {
+///             print(messaage)
+///         }
+///     }
+/// }
+/// ```
+public final class MessageQueue<Message: Sendable>: Sendable {
     
-    public typealias Handler = @Sendable (Value) async -> Void
-    
+    /// The type of closure used to handle dispatched messages.
+    public typealias Handler = @Sendable (Message) async -> Void
+
+    /// Creates a new, empty message queue with no observers.
     public init() {
+        let (stream, continuation) = AsyncStream.makeStream(of: Message.self)
+        self.continuation = continuation
+        self.observations = Observers(stream: stream)
     }
     
     /// Dispatch a message to all registered consumers.
-    nonisolated public func dispatch(_ value: Value) {
-        Task {
-            await self.emit(value)
-        }
+    public func dispatch(_ message: Message) {
+        continuation.yield(message)
     }
     
     /// Subscribe to messages on this `MessageQueue`.
@@ -27,9 +57,19 @@ public actor MessageQueue<Value: Sendable> {
     /// Registering a consumer in this way is _not_ unsubscribeable.
     ///
     /// Use this when the `MessageQueue` instance lifetime is shorter or the same as your consumer's.
-    nonisolated public func observe(_ handler: @escaping Handler) {
+    ///
+    /// > Ordering is _not_ guaranteed when calling this version of `observe` immediately followed by `dispatch`,
+    /// > like this:
+    /// > ```swift
+    /// > messageQueue.observe { print($0) }
+    /// > messageQueue.dispatch("hello")
+    /// > ```
+    /// > Because this version of `observe` is `nonisolated` and can be called from any context, it necessarily
+    /// > needs to make an async hop, and since `dispatch` needs to do the same, the observation _may_ actually
+    /// > occur _after_ the message is dispatched.
+    public func observe(_ handler: @escaping Handler) {
         Task {
-            await self.addHandler(handler)
+            await observations.addHandler(handler)
         }
     }
     
@@ -41,45 +81,71 @@ public actor MessageQueue<Value: Sendable> {
     ///
     /// - Returns: An `AnyCancellableAsync` which can be used to cancel the observation.
     public func observe(_ handler: @escaping Handler) async -> AnyCancellableAsync {
-        let id = self.addHandler(handler)
+        let id = await observations.addHandler(handler)
         return AnyCancellableAsync { [weak self] in
             guard let self else { return }
             Task.detached {
-                await self.removeHandler(id)
+                await self.observations.removeHandler(id)
             }
         }
     }
     
     // MARK: - Implementation details
     
-    private typealias ObservationId = UUID
+    private let continuation: AsyncStream<Message>.Continuation
     
-    private var handlers: [ObservationId: Handler] = [:]
+    private let observations: Observers
     
-    private func emit(_ value: Value) async {
-        Task.detached { [handlers] in
-            for handler in handlers.values {
-                await handler(value)
+    private actor Observers {
+        
+        var handlers: [UUID: Handler] = [:]
+        private var task: Task<Void, Never>?
+        private let stream: AsyncStream<Message>
+        
+        init(stream: AsyncStream<Message>) {
+            self.stream = stream
+        }
+        
+        deinit {
+            task?.cancel()
+        }
+        
+        private func emit(_ message: Message) async {
+            await withTaskGroup { group in
+                for handler in handlers.values {
+                    group.addTask {
+                        await handler(message)
+                    }
+                }
+            }
+        }
+        
+        @discardableResult
+        func addHandler(_ handler: @escaping Handler) -> UUID {
+            setUpTask()
+            let id = UUID()
+            handlers[id] = handler
+            return id
+        }
+        
+        func removeHandler(_ id: UUID) async {
+            handlers.removeValue(forKey: id)
+        }
+        
+        func setUpTask() {
+            guard task == nil else { return }
+            self.task = Task {
+                for await message in stream {
+                    await emit(message)
+                }
             }
         }
     }
     
-    @discardableResult
-    private func addHandler(_ handler: @escaping Handler) -> ObservationId {
-        let id = ObservationId()
-        handlers[id] = handler
-        return id
-    }
-    
-    private func removeHandler(_ id: ObservationId) async {
-        handlers.removeValue(forKey: id)
-    }
-    
-    #if DEBUG
+#if DEBUG
     // For unit test support
     func handlerCount() async -> Int {
-        handlers.count
+        await observations.handlers.count
     }
-    #endif
+#endif
 }
-
